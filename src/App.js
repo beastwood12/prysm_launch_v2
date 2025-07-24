227ac6b6439029c4b38bdd7b5bf482d78244a14e
beastwood12 <62308969+beastwood12@users.noreply.github.com>
2025-07-24 14:36:31 -0600

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PrysmForecastingModel from './components/PrysmForecastingModel';
+
+function App() {
+  return (
+    <div className="App">
+      <PrysmForecastingModel />
+    </div>
+  );
+}
+
+export default App;
